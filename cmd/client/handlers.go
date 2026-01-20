76239cef30e4aa5742612dae4ee6b45bc94197de
swokamoto <swokamoto@gmail.com>
2026-01-20 10:57:56 -0500

    handers moves

--- cmd/client/handlers.go
+++ cmd/client/handlers.go
@@ -7,10 +7,16 @@ import (
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 )
 
+func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) {
+	return func(move gamelogic.ArmyMove) {
+		defer fmt.Print("> ")
+		gs.HandleMove(move)
+	}
+}
+
 func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) {
 	return func(ps routing.PlayingState) {
 		defer fmt.Print("> ")
 		gs.HandlePause(ps)
 	}
 }
-
